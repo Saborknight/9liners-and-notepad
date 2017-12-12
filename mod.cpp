c6a438548424a85d56c7f1d76c8e6c0285acb3be
Saborknight <support@saborknight.com>
2017-12-12 00:05:50 +0000

    Bump v0.3.2

--- mod.cpp
+++ mod.cpp
@@ -2,4 +2,4 @@ name = "9Liners and Notepad";
 picture = "mod.paa";
 actionName = "Feedback";
 action = "http://forums.bistudio.com/member.php?237335-AWC_Chief_Wiggum";
-description = "Version: 0.3.1";
+description = "Version: 0.3.2";
